d593f5d1f1caa6d12cc7bf8da5eaead51570de26
JulianK123 <julian.kruk@stud.vilniustech.lt>
2023-11-15 23:27:19 +0200

    changed

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,24 +1,36 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 25,
+  breakpoints: {
+    990:{
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 25,
+    }
+      
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ 
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  autoplay: {
+      delay: 5000,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    autoplay: {
-        delay: 5000,
-      },
-  });
+});
